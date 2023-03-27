cf2aa0302276f70655d89919704ff142cb723f1c
victorshamo <37343169+victorshamo@users.noreply.github.com>
2023-03-27 13:34:08 +0100

    User Roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,13 +4,17 @@ const CustomError = require('../errors');
 
 
 const register = async (req, res) => {
-    const {email} = req.body;
+    const {email, name, password} = req.body;
     const emailAlreadyExists = await User.findOne({email})
     if(emailAlreadyExists){
         throw new CustomError.BadRequestError('Email already exists')
     }
-    
-    const user = await User.create(req.body);
+
+    // first refistered user is an admin
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount? 'admin':'user';
+
+    const user = await User.create({name,email,password,role});
     res.status(StatusCodes.CREATED).json({ user });
 };
 
