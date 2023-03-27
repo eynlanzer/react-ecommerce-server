2be4ca414b5e6e31c13ae76676e468ebdd4fd002
victorshamo <37343169+victorshamo@users.noreply.github.com>
2023-03-27 13:24:54 +0100

    Register - Unique Email

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,12 @@ const CustomError = require('../errors');
 
 
 const register = async (req, res) => {
+    const {email} = req.body;
+    const emailAlreadyExists = await User.findOne({email})
+    if(emailAlreadyExists){
+        throw new CustomError.BadRequestError('Email already exists')
+    }
+    
     const user = await User.create(req.body);
     res.status(StatusCodes.CREATED).json({ user });
 };
